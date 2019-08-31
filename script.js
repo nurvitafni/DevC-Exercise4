c758a91042b47e36aa52a7a9608bceaca34f57ea
nurvitafni <53710586+nurvitafni@users.noreply.github.com>
2019-08-31 15:33:24 +0700

    Adding js.

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.slider').slider({
+        indicators: false,
+        height: 500,
+        duration: 800,
+        interval: 2000
+    });
+    $('.carousel').carousel();
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
+  });
+      
